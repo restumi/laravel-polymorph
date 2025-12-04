67b1eb5b1b23221f95c1f9abfb81591dc19eaa1b
restumi <maulanaismael5@gmail.com>
2025-12-04 09:51:10 +0700

    create models & migrations

---
+++ app/Models/Comment.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    protected $fillable = [
+        'body',
+        'commentable_id',
+        'commentable_type'
+    ];
+
+    public function commentable()
+    {
+        return $this->morphTo();
+    }
+}
